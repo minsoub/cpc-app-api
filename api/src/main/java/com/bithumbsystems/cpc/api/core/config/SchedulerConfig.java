2a5a4da92e0cd9e9921cdc7f1b3d4fe9b4232dc8
sm <seoungmin104@bithumbsystems.com>
2022-11-03 16:55:35 +0900

    Fix: SchedulerConfig 변경

--- api/src/main/java/com/bithumbsystems/cpc/api/core/config/SchedulerConfig.java
+++ api/src/main/java/com/bithumbsystems/cpc/api/core/config/SchedulerConfig.java
@@ -1,9 +1,13 @@
 package com.bithumbsystems.cpc.api.core.config;
 
 
+import com.bithumbsystems.cpc.api.core.config.property.AwsProperties;
+import com.bithumbsystems.cpc.api.core.config.property.MongoProperties;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.mongo.reactivestreams.ReactiveStreamsMongoLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
@@ -12,24 +16,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
 @EnableScheduling
-@RequiredArgsConstructor
-@Profile("!local")
+@Data
+@Slf4j
+@Profile("dev|qa|prod|eks-dev")
+@Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
 public class SchedulerConfig {
 
-  private final ParameterStoreConfig config;
+  private final MongoProperties properties;
+
+  public SchedulerConfig(MongoProperties properties) {
+    this.properties = properties;
+  }
 
   @Bean
   public MongoClient mongoClient() {
-    return MongoClients.create("mongodb://" + config.getMongoProperties().getMongodbUrl() + ":" + config.getMongoProperties().getMongodbPort());
+    return MongoClients.create("mongodb://" + properties.getMongodbUrl() + ":" + properties.getMongodbPort());
   }
 
   @Bean
   @Profile("!local")
   public LockProvider lockProvider(MongoClient mongoClient) {
-    return new ReactiveStreamsMongoLockProvider(mongoClient.getDatabase(config.getMongoProperties().getMongodbName()));
+    return new ReactiveStreamsMongoLockProvider(mongoClient.getDatabase(properties.getMongodbName()));
   }
 
 }
+
+
