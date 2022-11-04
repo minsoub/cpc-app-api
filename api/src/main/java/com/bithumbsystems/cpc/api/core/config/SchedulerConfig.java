2732a3beab6d80d326c88d2f428b202846cccf5d
sm <seoungmin104@bithumbsystems.com>
2022-11-04 20:28:51 +0900

    Feat: 스케줄러 빈도수 변경

--- api/src/main/java/com/bithumbsystems/cpc/api/core/config/SchedulerConfig.java
+++ api/src/main/java/com/bithumbsystems/cpc/api/core/config/SchedulerConfig.java
@@ -1,18 +1,13 @@
 package com.bithumbsystems.cpc.api.core.config;
 
 
-import com.bithumbsystems.cpc.api.core.config.property.AwsProperties;
-import com.bithumbsystems.cpc.api.core.config.property.MongoProperties;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.mongo.reactivestreams.ReactiveStreamsMongoLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
