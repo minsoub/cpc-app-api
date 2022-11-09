a07d95fd77b5151ca42449ae1844a593d33db4ed
이승민 <seoungmin104@bithumbsystems.com>
2022-11-09 07:06:28 +0000

    Squashed commit of the following
    commit 561125df 
    Author: sm <seoungmin104@bithumbsystems.com> 
    Date: Wed Nov 09 2022 16:05:36 GMT+0900 (대한민국 표준시) 
    
         Feat: 공시 테이블 인덱스 추가 sort 삭제
    
    
    commit cd3be602 
    Author: sm <seoungmin104@bithumbsystems.com> 
    Date: Wed Nov 09 2022 14:37:13 GMT+0900 (대한민국 표준시) 
    
         Feat : 찾아가는 교육신청 비대칭 암호화 작업

--- persistence/mongodb/src/main/java/com/bithumbsystems/persistence/mongodb/disclosure/model/entity/Disclosure.java
+++ persistence/mongodb/src/main/java/com/bithumbsystems/persistence/mongodb/disclosure/model/entity/Disclosure.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -23,6 +24,7 @@ public class Disclosure extends Date {
 
   @Id
   private String id;
+  @Indexed
   private String projectSymbol;
   private String projectLogo;
   private String projectId;
